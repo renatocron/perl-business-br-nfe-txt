b8fb73f6af163d58239648167cb1c08cc553e4dd
renato <renato@aware.com.br>
2012-09-19 01:14:15 -0300

    versao 1 testada com RPS funcionando

--- lib/Business/BR/NFe/RPS/TXT.pm
+++ lib/Business/BR/NFe/RPS/TXT.pm
@@ -1,7 +1,7 @@
 package Business::BR::NFe::RPS::TXT;
 use Moose;
 use namespace::autoclean;
-
+use Carp;
 
 has data_ini => (
     is => 'ro',
@@ -15,10 +15,50 @@ has inscricao_municipal => (
     is => 'ro',
 );
 
+has _total_servico => (
+    is => 'rw',
+    isa => 'Num',
+    default => sub {0}
+);
 
-sub _pad_str {}
+has _total_deducao => (
+    is => 'rw',
+    isa => 'Num',
+    default => sub {0}
+);
 
-sub _pad_num {}
+has _rps_lines => (
+    is => 'rw',
+    isa => 'Str',
+    default => sub {''}
+);
+
+has _total_linhas => (
+    is => 'rw',
+    isa => 'Int',
+    default => sub {0}
+);
+
+sub _pad_str {
+    my ($str, $size)=@_;
+    return $str . ' ' x ($size - length($str));
+}
+
+sub _pad_num {
+    my ($num, $size, $round) = @_;
+
+    if ($round && $round =~ /^[0-9]$/){
+        $round = int ('1' . '0' x ($round-1));
+    }else{
+        $round = 1;
+    }
+    $num = $num * $round;
+    if (ref $size eq 'ARRAY'){
+        croak "Not a number";
+    }else{
+        return sprintf( '%0'.$size.'s', $num * $round );
+    }
+}
 
 
 sub adiciona_rps {
@@ -36,7 +76,7 @@ sub adiciona_rps {
         serie  => ['str', 5],
         numero => ['num', 12],
         emissao => ['date'],
-        situacao => ['str', 1],
+        situacao => ['str', 1, ['T','I','F','C', 'E', 'J']],
         valor_servico => ['num', 15, 2],
         valor_deducao => ['num', 15, 2],
         codigo_servico => ['num', 5],
@@ -45,7 +85,7 @@ sub adiciona_rps {
         cpf_cnpj_flag => ['num', [1,2,3]],
         cpf_cnpj      => ['num', 14],
         inscricao_municipal => ['num', 8],
-        inscricao_estadual => ['num', 8],
+        inscricao_estadual => ['num', 12],
         razao_social => ['str', 75],
         endereco_tipo => ['str', 3],
         endereco => ['str', 50],
@@ -58,13 +98,125 @@ sub adiciona_rps {
         email => ['str', 75],
         discriminacao => ['str', -1000],
     };
+    $params{aliquota} = $params{aliquota} * 100 if $params{aliquota};
+    my $out = $self->_validate($campos , %params);
+    my @ordem = qw/
+        serie
+        numero
+        emissao
+        situacao
+        valor_servico
+        valor_deducao
+        codigo_servico
+        aliquota
+        iss_retido
+        cpf_cnpj_flag
+        cpf_cnpj
+        inscricao_municipal
+        inscricao_estadual
+        razao_social
+        endereco_tipo
+        endereco
+        endereco_num
+        endereco_complemento
+        endereco_bairro
+        endereco_cidade
+        endereco_uf
+        endereco_cep
+        email
+        discriminacao/;
+    my $line = '2'; # registro 2 versao 001
+    $line .= _pad_str('RPS', 5);
+    foreach (@ordem){
+        $line .= $out->{$_};
+    };
+    $line .= "\r\n";
+    use DDP; p $out;
 
+    $self->_rps_lines($self->_rps_lines . $line);
 
+    $self->_total_deducao( $self->_total_deducao + $params{valor_deducao});
+    $self->_total_servico( $self->_total_servico + $params{valor_servico});
+
+    $self->_total_linhas($self->_total_linhas + 1);
+    return 1;
 }
 
+sub _validate {
+    my ($self, $config, %params) = @_;
+    my $x={};
+    foreach my $campo (keys %$config){
+        my $ref = $config->{$campo};
+        next unless ref $ref eq 'ARRAY';
+
+        croak "$campo nao foi enviado" unless defined $params{$campo};
+
+        if ($ref->[0] eq 'str'){
+            my $size = $ref->[1];
+
+            if ($size > 0 && length($params{$campo}) > $size){
+                croak "$campo maior que $size";
+            }elsif ($size < 0){
+                $size = $size * -1;
+                if (length($params{$campo}) > $size){
+                    croak "$campo maior que $size";
+                }
+                $x->{$campo} = $params{$campo};
+            }else{
+                $x->{$campo} = _pad_str($params{$campo}, $size);
+            }
+
+        }elsif ($ref->[0] eq 'num'){
+            $params{$campo} ||= 0;
+            if ($params{$campo} !~ /^[0-9]+(?:\.[0-9]+)?$/){
+                croak "$campo com valor $params{$campo} nao eh um numero";
+            }else{
+                my $size = $ref->[1];
+                my $round = $ref->[2];
+                if (ref $size eq 'ARRAY'){
+                    my %valid = map {$_=>1} @$size;
+                    croak "$campo invalido " unless $valid{$params{$campo}};
+                    $size = 1; # WARNING nao era pra ser assim..
+                }elsif (length($params{$campo}) > $size){
+                    croak "$campo maior que $size posicoes";
+                }
+                $x->{$campo} = _pad_num($params{$campo}, $size, $round);
+            }
+
+        }elsif ($ref->[0] eq 'date'){
+            if ($params{$campo} !~ /^\d{4}\d{2}\d{2}$/){
+                croak "$campo nao esta no formato AAAAMMDD";
+            }else{
+                $x->{$campo} = $params{$campo};
+            }
+        }
+    }
+    return $x;
+}
 
 sub salva_txt {
+    my ($self) = @_;
 
+    my $campos = {
+        data_fim  => ['date'],
+        data_ini => ['date'],
+        inscricao_municipal=> ['num', 8],
+    };
 
+    my $out = $self->_validate($campos,
+        data_fim => $self->data_fim,
+        data_ini => $self->data_ini,
+        inscricao_municipal => $self->inscricao_municipal);
 
+    my $str = '1001' . $out->{inscricao_municipal} . $out->{data_ini}.$out->{data_fim} . "\r\n" . $self->_rps_lines;
+
+    $str .= '9' .
+        _pad_num($self->_total_linhas, 7) .
+        _pad_num($self->_total_servico, 15, 2) .
+        _pad_num($self->_total_deducao, 15, 2) . "\r\n";
+
+    return $str;
 }
+
+1
+
